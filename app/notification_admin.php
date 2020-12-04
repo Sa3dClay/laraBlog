33fd9b36982b1bcadd2db1ee6533f5f34b871f4b
Sa3dClay <sa3d.clay@gmail.com>
2020-12-04 17:23:51 +0200

    organize code

--- app/notification_admin.php
+++ app/notification_admin.php
@@ -6,23 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification_admin extends Model
 {
-  /// Table Name
-  protected $table = 'notifications_admin';
-  // there's no primary key ,it's a weak entity
+    protected $table = 'notifications_admin';
+    // there's no primary key, it's a weak entity
 
-  // Timestamps
-  public $timestamps = true;
+    // Timestamps
+    public $timestamps = true;
 
-  public function post() {
-      return $this->belongsTo('App\Post', 'post_id');
-  }
+    public function post()
+    {
+        return $this->belongsTo('App\Post', 'post_id');
+    }
 
-  public function feedback() {
-      return $this->belongsTo('App\Feedback', 'feedback_id');
-  }
+    public function feedback()
+    {
+        return $this->belongsTo('App\Feedback', 'feedback_id');
+    }
 
-  //Notification will be sent to all admins
-  /*public function user() {
-      return $this->belongsTo('App\Admin', 'admin_id');
-  }*/
+    // Notification will be sent to all admins
+    /* public function user() {
+        return $this->belongsTo('App\Admin', 'admin_id');
+    } */
 }
