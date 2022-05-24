dc59458f8705ea24a1c07d14342039ac629a4ac2
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-05-24 21:43:32 +0200

    fix min height issue

--- public/js/app.js
+++ public/js/app.js
@@ -75,17 +75,7 @@ module.exports = __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-$(function () {
-    $("body").niceScroll({
-        cursorcolor: "#3498db",
-        cursorwidth: "8px",
-        cursoropacitymin: 0.4,
-        cursoropacitymax: 0.8
-    });
-    $("body").mouseover(function () {
-        $("body").getNiceScroll().resize();
-    });
-});
+document.getElementById("app").style.minHeight = window.outerHeight - 116 + 'px';
 
 /***/ }),
 /* 2 */
