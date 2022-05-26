2f53036614e0d9cf2db3e1884fe822c2b46f118b
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-05-26 13:05:06 +0200

    Development (#22)
    * add app.css tracking
    
    * add admin seeder
    
    Co-authored-by: abdulrahman atef <abdulrahman.atef@iken.tech>

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'id'        => 1,
+            'name'      => "Admin",
+            'email'     => "Admin@lara.com",
+            'password'  => Hash::make('p@ssw0rd'),
+        ]);
+    }
+}
