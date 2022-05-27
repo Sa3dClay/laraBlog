091bff71f3d78bd1c9672fb9a58274cf08749159
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-05-27 21:34:12 +0200

    update about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,37 +4,33 @@
 
     <div class="about-us jumbotron my-2 py-2">
         <div class="row justify-content-around">
-            <div class="col-1"></div>
-            <div class="col-2 about-img">
+            <div class="col-md-4 about-img p-5">
                 <img src="{{ asset('img/atef.jpg') }}" class="img-fluid" title="Abdo Atef" />
             </div>
 
-            <div class="col-8 about-info">
-                <h2 class="name">
+            <div class="col-md-8 about-info">
+                <h2 class="name text-center">
                     <span id="name1" class="typewriter"></span>
                 </h2>
-                <span class="bio">Full-stack Web Developer / Database Developer</span>
+                <span class="bio">Software Engineer (C++) / Full-stack Developer (Laravel, Vue)</span>
                 <p>I'm a programmer who loves coding, working in web development and have a few experiences about UI/UX, building web applications with Laravel & Vue, building DBs using oracle/Mysql. </p>
             </div>
-            <div class="col-1"></div>
         </div>
 
         <hr>
 
         <div class="row justify-content-around">
-            <div class="col-1"></div>
-            <div class="col-8 about-info">
-                <h2 class="name">
+            <div class="col-md-8 about-info">
+                <h2 class="name text-center">
                     <span id="name2" class="typewriter"></span>
                 </h2>
-                <span class="bio">Back-end Web / Desktop App Developer / Database Developer</span>
+                <span class="bio">Software Engineer (Java) / Back-end Developer (Laravel)</span>
                 <p>I'm a programmer who loves coding, working in web development/Desktop app development, building web applications with Laravel/desktop apps with Java, building DBs using oracle/Mysql.</p>
             </div>
 
-            <div class="col-2 about-img">
+            <div class="col-md-4 about-img p-5">
                 <img src="{{ asset('img/sobh.jpg') }}" class="img-fluid" title="Abdo Sobh" />
             </div>
-            <div class="col-1"></div>
         </div>
     </div>
 
@@ -49,7 +45,7 @@
         $(function () {
             var i = 0,
                 j = 0,
-                speed = 140,
+                speed = 150,
                 name1text = 'Abdulrahman Atef',
                 name2text = 'Abdulrahman Sobh',
                 name1 = document.getElementById("name1"),
